071cfc945ddfb4c2feedb25db25dff576ea6fa1f
Roses_Nguyen <nngklinh.2911@gmail.com>
2026-03-11 00:02:12 -0400

    Apply TourismTrait to France, Vietnam, Japan, and Canada

--- includes/classes/Country/AmericanCountry/Canada/Canada.php
+++ includes/classes/Country/AmericanCountry/Canada/Canada.php
@@ -3,9 +3,11 @@
 namespace MyProject\Country\AmericanCountry;
 
 use MyProject\Country\AmericanCountry;
+use MyProject\TourismTrait;
 
 // Represents Canada.
 
 class Canada extends AmericanCountry
 {
+    use TourismTrait;
 }
--- includes/classes/Country/EuropeanCountry/France/France.php
+++ includes/classes/Country/EuropeanCountry/France/France.php
@@ -3,9 +3,11 @@
 namespace MyProject\Country\EuropeanCountry;
 
 use MyProject\Country\EuropeanCountry;
+use MyProject\TourismTrait;
 
 // Represents France.
 
 class France extends EuropeanCountry
 {
+    use TourismTrait;
 }
